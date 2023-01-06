611fe28d178fdeb7f394adb76f4eec0621db5991
José Nery <josenerydev@gmail.com>
2023-01-05 21:38:35 -0300

    Add project files.

---
+++ HealthCheckAPI/Program.cs
@@ -0,0 +1,42 @@
+global using HealthCheckAPI;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors();
+
+// Add services to the container.
+
+builder.Services.AddHealthChecks()
+    .AddCheck("ICMP_01",
+        new ICMPHealthCheck("www.ryadel.com", 100))
+    .AddCheck("ICMP_02",
+        new ICMPHealthCheck("www.google.com", 100))
+    .AddCheck("ICMP_03",
+        new ICMPHealthCheck($"www.{Guid.NewGuid():N}.com", 100));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors();
+
+app.UseAuthorization();
+
+app.UseHealthChecks(new PathString("/api/health"),
+    new CustomHealthCheckOptions());
+
+app.MapControllers();
+
+app.Run();
